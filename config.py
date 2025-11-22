ca3f678d685f81be7851a78fa95f8c94a40da6c5
BOT_TG_0000000 <143434573+igor-2004@users.noreply.github.com>
2025-11-22 08:19:20 +0300

    Update config.py

--- config.py
+++ config.py
@@ -1,15 +1,15 @@
 import os
 
 # Обязательные: задаются как переменные окружения на Render или в .env при локальной разработке
-BOT_TOKEN = os.environ.get("BOT_TOKEN")  # ОБЯЗАТЕЛЬНО
+BOT_TOKEN = os.environ.get("8073733884:AAHRpXo9yZ3LTGeaYJD03fuzx1vRChlpa4k")  # ОБЯЗАТЕЛЬНО
 if not BOT_TOKEN:
     raise RuntimeError("BOT_TOKEN not set in environment!")
 
 # ID канала или чат-ид для публикации (например: -1001234567890)
-CHANNEL_ID = int(os.environ.get("CHANNEL_ID", "0"))
+CHANNEL_ID = int(os.environ.get("CHANNEL_ID", "-1001958513038"))
 
 # ID владельца/админа (Telegram user id)
-OWNER_ID = int(os.environ.get("OWNER_ID", "0"))
+OWNER_ID = int(os.environ.get("OWNER_ID", "1184497918"))
 
 # Путь к sqlite (если используете Render disk — укажите /data/bot_database.db)
 DB_PATH = os.environ.get("DB_PATH", "/data/bot_database.db")
